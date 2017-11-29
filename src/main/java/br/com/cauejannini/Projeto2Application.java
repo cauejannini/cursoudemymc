1eb91bf780bb8f6ce86c082862f575d7badf6bd3
Cauê Jannini <cauejannini@gmail.com>
2017-11-29 15:16:21 -0200

    Criando operação de instanciação

--- src/main/java/br/com/cauejannini/Projeto2Application.java
+++ src/main/java/br/com/cauejannini/Projeto2Application.java
@@ -1,12 +1,32 @@
 package br.com.cauejannini;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Projeto2Application {
+import br.com.cauejannini.domain.Categoria;
+import br.com.cauejannini.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class Projeto2Application implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Projeto2Application.class, args);
+	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática"); 
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.save(Arrays.asList(cat1, cat2));
+		
 	}
 }
